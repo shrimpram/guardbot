7f37c33340966bc6757c6e11a6e57c8b7473c3a0
Shreeram Modi <smodi@smodi.net>
2025-07-12 17:36:51 -0400

    env: initialize flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              black
+
+              python313
+              python313Packages.python-dotenv
+              python313Packages.slack-sdk
+            ];
+          };
+      }
+    );
+}
